054c5b4f233b4e85b7d2666204da67c260e2a2f9
Crowdbotics <team@crowdbotics.com>
2020-10-29 18:29:32 +0000

    Update models

--- backend/madison_nj_covid19__22096/wsgi.py
+++ backend/madison_nj_covid19__22096/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'madison_nj_covid19__22096.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "madison_nj_covid19__22096.settings")
 
 application = get_wsgi_application()
